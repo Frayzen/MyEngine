69d7db316e77a291fdc381afb53e3da612a2fe0c
florian.laine <florian.laine@epita.fr>
2025-07-17 17:02:30 +0200

    feat: imgui is a submodule now

--- include/window.hh
+++ include/window.hh
@@ -1,15 +1,17 @@
 #pragma once
 
 #include "glad/glad.h"
+#include "interface.hh"
 #include "scene.hh"
 #include <GLFW/glfw3.h>
 
 class Window {
 public:
-  int setupWindow(int width, int height, const char *title);
+  Window(int width, int height, const char *title);
   void run(Scene &scene);
   ~Window();
 
 private:
   GLFWwindow *window;
+  std::shared_ptr<Interface> interface;
 };
